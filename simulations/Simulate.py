a8fce108d076fecab38cbe8f5a513f23331ad6df
Milena Musial <m.musial@outlook.de>
2024-05-29 15:55:46 +0200

    no warning before test
    Former-commit-id: c4e0124f38346dd6d31032f544e6b84406d03a78

--- simulations/Simulate.py
+++ simulations/Simulate.py
@@ -45,14 +45,14 @@ def simulate(model, condition, sim_num, output_filename, full_logging=True):
 
     sim_num_list = []
 
-    for i in range(sim_num):
-    # Initialize the simulation-specific parameters
+    for i in range(sim_num): # simulation loop
+        # Initialize the simulation-specific parameters
         transitions = [[2, 3], [4, 5], [5, 6], [7], [8], [9], [10], [10], [10], [11]]
 
-    if condition == "Policy":
-        rewards = [[0, 0], [0, 0], [0, 0], [0], [0], [0], [10], [20], [30], [0]]
-    else:
-        rewards = [[0, 0], [0, 0], [0, 0], [0], [0], [0], [20], [0], [30], [0]]
+        if condition == "Policy":
+            rewards = [[0, 0], [0, 0], [0, 0], [0], [0], [0], [10], [20], [30], [0]]
+        else:
+            rewards = [[0, 0], [0, 0], [0, 0], [0], [0], [0], [20], [0], [30], [0]]
 
         if model == "Punctate":
             v_state = []
@@ -74,7 +74,7 @@ def simulate(model, condition, sim_num, output_filename, full_logging=True):
             init_t_counts = np.zeros((num_pairs, num_states))
             model_parameters = [v_state, init_t_counts, init_weight]
         else:
-            init_sr = np.identity((num_pairs)) # init M with identity matrix as in Russek et al. 2017
+            init_sr = np.identity(num_pairs) # init M with identity matrix as in Russek et al. 2017
             init_sr[-1,:] = 0 # set row corresponding to terminal state to 0 as in Russek et al. 2017
             init_weight = np.zeros(num_pairs)
             
@@ -102,20 +102,20 @@ def simulate(model, condition, sim_num, output_filename, full_logging=True):
         for l in range(len(epi_length_pretraining) + len(epi_length_retraining)):
             sim_num_list.append(i+1)
 
-    # Action name formatting: VS1A3 = action going from state 1 to state 3
-    move_names = ['S' + str(index+1) + 'A' + str(i) for index, k in enumerate(transitions) for i in k]
+        # Action name formatting: VS1A3 = action going from state 1 to state 3
+        move_names = ['S' + str(index+1) + 'A' + str(i) for index, k in enumerate(transitions) for i in k]
 
-    # Occupancy name formatting: OS1A2-S2A4 = future occupancy of the S2-A4 action given the S1-A2 action
-    occupancy_names = ['O' + i + '-' + j for i in move_names for j in move_names]
+        # Occupancy name formatting: OS1A2-S2A4 = future occupancy of the S2-A4 action given the S1-A2 action
+        occupancy_names = ['O' + i + '-' + j for i in move_names for j in move_names]
 
-    # Transition name formatting: TS1A2 - S2 = probability of transitioning from state 1 to state 2 given the S1-A2 action
-    transition_names = ['T' + i + '-S' + str(j + 1) for i in move_names for j in range(num_states)]
+        # Transition name formatting: TS1A2 - S2 = probability of transitioning from state 1 to state 2 given the S1-A2 action
+        transition_names = ['T' + i + '-S' + str(j + 1) for i in move_names for j in range(num_states)]
 
-    # Save the individual time-step logs
-    if full_logging == True:
-        result = pd.DataFrame({'Model': model, 'Condition': condition, 'Simulation': sim_num_list, 'Phase': logging_lists[4], 'Episode': logging_lists[3], \
-            'State': logging_lists[0], 'Action': logging_lists[1], 'RPE': logging_lists[2]})
-    
+        # Save the individual time-step logs
+        if full_logging == True:
+            result = pd.DataFrame({'Model': model, 'Condition': condition, 'Simulation': sim_num_list, 'Phase': logging_lists[4], 'Episode': logging_lists[3], \
+                'State': logging_lists[0], 'Action': logging_lists[1], 'RPE': logging_lists[2]})
+
         # Add each action's calculated values to the logging frame
         values = {}
         for index, j in enumerate(move_names):
@@ -124,34 +124,34 @@ def simulate(model, condition, sim_num, output_filename, full_logging=True):
 
         result.to_csv(output_filename)
 
-    # Save the milestone logs
-    milestone_results = pd.DataFrame({'Simulation': milestone_labels[0], 'Phase': milestone_labels[1]})
-    if model == "Punctate":
-        # The punctate model saves the Q-value of each state-action pair
-        values = {}
-        for index, j in enumerate(move_names):
-            values['V' + j] = milestone_logs[index]
-        milestone_results = pd.concat((milestone_results, pd.DataFrame(values)), axis=1)
-    elif model == "ModelBased":
-        # The model-based model saves the weight vector and a flattened transition matrix
-        weights_and_transitions = {}
-        for index, j in enumerate(move_names):
-            weights_and_transitions["W" + j] = milestone_logs[index]
-        for index, j in enumerate(transition_names):
-            weights_and_transitions[j] = milestone_logs[index + num_pairs]
-        milestone_results = pd.concat((milestone_results, pd.DataFrame(weights_and_transitions)), axis=1)
-    else:
-        # The SR models save the weight vector and a flattened successor matrix
-        weights_and_sr = {}
-        for index, j in enumerate(move_names):
-            weights_and_sr["W" + j] = milestone_logs[index]
-        for index, j in enumerate(occupancy_names):
-            weights_and_sr[j] = milestone_logs[index + num_pairs]
-        milestone_results = pd.concat((milestone_results, pd.DataFrame(weights_and_sr)), axis=1)
+        # Save the milestone logs
+        milestone_results = pd.DataFrame({'Simulation': milestone_labels[0], 'Phase': milestone_labels[1]})
+        if model == "Punctate":
+            # The punctate model saves the Q-value of each state-action pair
+            values = {}
+            for index, j in enumerate(move_names):
+                values['V' + j] = milestone_logs[index]
+            milestone_results = pd.concat((milestone_results, pd.DataFrame(values)), axis=1)
+        elif model == "ModelBased":
+            # The model-based model saves the weight vector and a flattened transition matrix
+            weights_and_transitions = {}
+            for index, j in enumerate(move_names):
+                weights_and_transitions["W" + j] = milestone_logs[index]
+            for index, j in enumerate(transition_names):
+                weights_and_transitions[j] = milestone_logs[index + num_pairs]
+            milestone_results = pd.concat((milestone_results, pd.DataFrame(weights_and_transitions)), axis=1)
+        else:
+            # The SR models save the weight vector and a flattened successor matrix
+            weights_and_sr = {}
+            for index, j in enumerate(move_names):
+                weights_and_sr["W" + j] = milestone_logs[index]
+            for index, j in enumerate(occupancy_names):
+                weights_and_sr[j] = milestone_logs[index + num_pairs]
+            milestone_results = pd.concat((milestone_results, pd.DataFrame(weights_and_sr)), axis=1)
 
-    milestone_results['Test Result'] = test_result_list
+        milestone_results['Test Result'] = test_result_list
 
-    milestone_results.to_csv(output_filename[:-4] + "_Results.csv")
+        milestone_results.to_csv(output_filename[:-4] + "_Results.csv")
 
     return milestone_results
         
